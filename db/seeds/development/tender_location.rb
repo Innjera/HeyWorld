52582cb52c649c6df3ae41660b8f70d9d7b12c40
Yu Sagawa <yu.sagawa1115.africaunite@gmail.com>
2019-02-02 21:17:07 +0900

    modified bug

--- db/seeds/development/tender_location.rb
+++ db/seeds/development/tender_location.rb
@@ -1,39 +1,56 @@
-company_names = [
-  "株式会社コマゼン",
-  "株式会社ユーパーツ",
-  "株式会社エコアール",
-  "株式会社吉田商会"
-]
+seller1 = Seller.find_by(company_name: "株式会社コマゼン")
+TenderLocation.create!(
+  seller: seller1,
+  address_prefecture_part: "Aichi",
+  address_city_part: "Yatomi",
+  address_rest_part: "Kusunoki, 1-26-2"
+)
+TenderLocation.create!(
+  seller: seller1,
+  address_prefecture_part: "Tokyo(TEST1)",
+  address_city_part: "Minatoku(TEST1)",
+  address_rest_part: "Roppongi(TEST1) 1-2-3"
+)
 
-address_prefecture_parts = [
-  "Aichi",
-  "Saitama",
-  "Osaka",
-  "Kanagawa"
-]
 
-address_city_parts = [
-  "Nagoya",
-  "Yokohama",
-  "Osaka",
-  "Yatomi"
-]
+seller2 = Seller.find_by(company_name: "株式会社ユーパーツ")
+TenderLocation.create!(
+  seller: seller2,
+  address_prefecture_part: "Saitama",
+  address_city_part: "Kumagaya",
+  address_rest_part: "Sayada 1285-2"
+)
+TenderLocation.create!(
+  seller: seller2,
+  address_prefecture_part: "Tokyo(TEST2)",
+  address_city_part: "Minatoku(TEST2)",
+  address_rest_part: "Roppongi(TEST2) 4-5-6"
+)
 
-address_rest_parts = [
-  "Kusunoki 1-26-2",
-  "Sayada 1285-2",
-  "Kubotacho 838-1",
-  "Shimojimachi 29"
-]
+seller3 = Seller.find_by(company_name: "株式会社吉田商会")
+TenderLocation.create!(
+  seller: seller3,
+  address_prefecture_part: "Aichi",
+  address_city_part: "Toyohashi",
+  address_rest_part: "Shimojimachi 29"
+)
+TenderLocation.create!(
+  seller: seller3,
+  address_prefecture_part: "Tokyo(TEST3)",
+  address_city_part: "Minatoku(TEST3)",
+  address_rest_part: "Roppongi(TEST3) 7-8-9"
+)
 
-company_names.each do |seller|
-  seller = Seller.find_by(company_name: seller)
-  0.upto(1) do |idx|
-    TenderLocation.create!(
-      seller: seller,
-      address_prefecture_part: address_prefecture_parts.sample,
-      address_city_part: address_city_parts.sample,
-      address_rest_part: address_rest_parts.sample
-    )
-  end
-end
+seller4 = Seller.find_by(company_name: "株式会社エコアール")
+TenderLocation.create!(
+  seller: seller4,
+  address_prefecture_part: "Tochigi",
+  address_city_part: "Ashikaga",
+  address_rest_part: "Kubotacho 838-1"
+)
+TenderLocation.create!(
+  seller: seller4,
+  address_prefecture_part: "Tokyo(TEST4)",
+  address_city_part: "Minatoku(TEST4)",
+  address_rest_part: "Roppongi(TEST4) 10-11-12"
+)
