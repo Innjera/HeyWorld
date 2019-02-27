434132be3c83fe3c1f72db4d96f0de13739ba79e
Yu Sagawa <yu.sagawa1115.africaunite@gmail.com>
2019-02-27 16:07:08 +0900

    added tender status

--- app/helpers/tender_status_helper.rb
+++ app/helpers/tender_status_helper.rb
@@ -5,34 +5,34 @@ module TenderStatusHelper
   T2 = T0.advance(hours: 24)
 
   def tender_status_text(tender)
-    ### 0(ドラフト)
+    ### 0(draft)
     if tender.preparation_status == 0
-      ### 現在より入札開始日が未来
-      if T1 < tender.starts_at
+      ### before tender
+      if tender.tender_status == 0
         content_tag(:span, class:"text-danger", style:"font-size:1rem;") do
           t('tenders.draft_text')
         end
-        ### 入札開始予定日を過ぎているのにまだステータスがドラフトの場合
-      elsif tender.starts_at < T1
+      ### tender ongoing or finished though tender is draft
+      elsif tender.tender_status == 1 || 2
         content_tag(:span, class:"badge badge-pill badge-warning py-1", style:"font-size:1rem;") do
           content_tag(:i, class: "fas fa-exclamation-triangle") do
             t('tenders.warn_text')
           end
         end
       end
-    ### 1(準備完了)
-  elsif tender.preparation_status == 1
-      ### 現在より入札開始日が未来
-      if T1 < tender.starts_at
+    ### 1(ready)
+    elsif tender.preparation_status == 1
+      ### before_tender
+      if tender.tender_status == 0
         content_tag(:span, class:"text-success", style:"font-size:1rem;") do
           t("tenders.ready_text")
         end
-    ### 入札中
-    elsif tender.starts_at < T1 && T1 < tender.ends_at
+      ### tender ongoing
+    elsif tender.tender_status == 1
       content_tag(:span, class:"text-primary", style:"font-size:1rem;") do
         t('tenders.in_progress')
       end
-    elsif tender.ends_at < T1
+    elsif tender.tender_status == 2
       content_tag(:span, class:"text-secondary", style:"font-size:1rem;") do
         t('tenders.completed')
       end
