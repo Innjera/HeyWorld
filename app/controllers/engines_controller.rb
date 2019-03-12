e1878c0c25c48dde0aa891ab8000dd3096c031b5
Yu Sagawa <yu.sagawa1115.africaunite@gmail.com>
2019-03-12 14:09:49 +0900

    modification

--- app/controllers/engines_controller.rb
+++ app/controllers/engines_controller.rb
@@ -1,6 +1,5 @@
 class EnginesController < ApplicationController
   def show
-    error_messages = flash[:error_messages] if error_messages.present?
     @engine = Engine.find(params[:id])
     @tender_rule = @engine.seller.tender_rule
     @bid_price = BidPrice.new
