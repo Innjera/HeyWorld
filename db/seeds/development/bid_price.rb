e9052c1348a9eaa9755919cb40340d5f9502ce18
Yu Sagawa <yu.sagawa1115.africaunite@gmail.com>
2019-03-15 10:55:07 +0900

    modified seed file of bid_price : delete create! & changed to new & save to accommodate "validate: false" method

--- db/seeds/development/bid_price.rb
+++ db/seeds/development/bid_price.rb
@@ -2,17 +2,21 @@ engines = Engine.joins(:tender).where('preparation_status= ?', "1")
 open_engines = engines.joins(:tender).where('starts_at <= ?', Time.current)
 
 1.upto(15) do |idx|
-  BidPrice.create!(
+  bid_price = BidPrice.new(
     user_id: 1,
     engine_id: open_engines.pluck(:id).fetch(idx),
-    price: 9000 * idx
+    price: 9000 * idx,
   )
+
+  bid_price.save!(validate: false)
 end
 
 1.upto(15) do |idx|
-  BidPrice.create!(
+  bid_price = BidPrice.new(
     user_id: 2,
     engine_id: open_engines.pluck(:id).fetch(idx),
-    price: 20000 * idx
+    price: 20000 * idx,
   )
+
+  bid_price.save!(validate: false)  
 end
