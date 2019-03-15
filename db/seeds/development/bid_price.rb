dbdd7af623991048bc5c965c198e37d287f0753b
Yu Sagawa <yu.sagawa1115.africaunite@gmail.com>
2019-03-15 14:43:41 +0900

    modified seed file

--- db/seeds/development/bid_price.rb
+++ db/seeds/development/bid_price.rb
@@ -1,22 +1,18 @@
 engines = Engine.joins(:tender).where('preparation_status= ?', "1")
-open_engines = engines.joins(:tender).where('starts_at <= ?', Time.current)
+open_engines = engines.joins(:tender).where('starts_at <= ? AND ends_at >= ?', Time.current, Time.current)
 
 1.upto(15) do |idx|
-  bid_price = BidPrice.new(
+  BidPrice.create!(
     user_id: 1,
     engine_id: open_engines.pluck(:id).fetch(idx),
-    price: 9000 * idx,
+    price: 90000 * idx
   )
-
-  bid_price.save!(validate: false)
 end
 
 1.upto(15) do |idx|
-  bid_price = BidPrice.new(
+  BidPrice.create!(
     user_id: 2,
     engine_id: open_engines.pluck(:id).fetch(idx),
-    price: 20000 * idx,
+    price: 200000 * idx
   )
-
-  bid_price.save!(validate: false)  
 end
