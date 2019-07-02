40ad1b7273dd7be81402b32131be67b56a719f84
innjera <yu.sagawa1115.africaunite@gmail.com>
2019-07-02 09:49:09 +0900

    redirect 先を変更

--- app/controllers/corporate_controller.rb
+++ app/controllers/corporate_controller.rb
@@ -2,6 +2,6 @@ class CorporateController < ApplicationController
   layout 'corporate'
 
   def top
-    redirect_to "https://www.seibii.com"
+    redirect_to "https://www.seibii.wok/corporate"
   end
 end
