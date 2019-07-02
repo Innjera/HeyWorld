c1501bc54aebd4a32e17062e7c581d62d32a2215
innjera <yu.sagawa1115.africaunite@gmail.com>
2019-07-02 09:51:50 +0900

    ミスを修正

--- app/controllers/corporate_controller.rb
+++ app/controllers/corporate_controller.rb
@@ -2,6 +2,6 @@ class CorporateController < ApplicationController
   layout 'corporate'
 
   def top
-    redirect_to "https://www.seibii.wok/corporate"
+    redirect_to "https://www.seibii.work/corporate"
   end
 end
